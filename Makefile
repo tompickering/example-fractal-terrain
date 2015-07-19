befd7ff0c4064d98a932f6667de5047eff06bb02
Tom Pickering <incorrectbaboon@gmail.com>
2015-07-19 17:40:11 +0100

    [IMP] Triangle splitting is done properly.

--- Makefile
+++ Makefile
@@ -1,2 +1,2 @@
 all:
-	g++ main.cpp -o prog -I/usr/X11R6/include -L/usr/X11R6/lib `sdl-config --libs --cflags` -lGL -lGLU
+	g++ -g main.cpp -o prog -I/usr/X11R6/include -L/usr/X11R6/lib `sdl-config --libs --cflags` -lGL -lGLU
--- main.cpp
+++ main.cpp
@@ -1,3 +1,4 @@
+#include <iostream>
 #include <vector>
 #include <cstdlib>
 #include <cstdio>
@@ -8,12 +9,15 @@
 #define WIDTH  640
 #define HEIGHT 480
 
+using std::cout;
+using std::endl;
 using std::vector;
 
 typedef struct {
   float x;
   float y;
   float z;
+  bool is_new;
 } Point3f;
 
 const float BASE_SIZE = 3.5f;
@@ -47,9 +51,9 @@ void init() {
   glLightfv(GL_LIGHT0, GL_DIFFUSE, dif);
   glLightfv(GL_LIGHT0, GL_AMBIENT, amb);
 
-  Point3f p0; p0.x =        0.0; p0.y = BASE_SIZE *  r3 / 2.0; p0.z = 0.0;
-  Point3f p1; p1.x = -BASE_SIZE; p1.y = BASE_SIZE * -r3 / 2.0; p1.z = 0.0;
-  Point3f p2; p2.x =  BASE_SIZE; p2.y = BASE_SIZE * -r3 / 2.0; p2.z = 0.0;
+  Point3f p0; p0.x =        0.0; p0.y = BASE_SIZE *  r3 / 2.0; p0.z = 0.0; p0.is_new = false;
+  Point3f p1; p1.x = -BASE_SIZE; p1.y = BASE_SIZE * -r3 / 2.0; p1.z = 0.0; p1.is_new = false;
+  Point3f p2; p2.x =  BASE_SIZE; p2.y = BASE_SIZE * -r3 / 2.0; p2.z = 0.0; p2.is_new = false;
 
   tri.push_back(p0);
   tri.push_back(p1);
@@ -71,7 +75,7 @@ Point3f extract(Point3f* ref) {
 }
 
 void print_point(Point3f p) {
-  printf("X %+5.5f, Y %+5.5f, Z %+5.5f\n", p.x, p.y, p.z);
+  printf("X %+5.5f, Y %+5.5f, Z %+5.5f, N %d\n", p.x, p.y, p.z, p.is_new);
 }
 
 void print_tri() {
@@ -85,40 +89,98 @@ void print_tri() {
   }
 }
 
+// Calculates 'n' for the triangle, where 'n' is the number of
+// points along a side.
+int calc_base(int size) {
+    int total = 0;
+    int base = 0;
+    for (int i = 1; total < size; ++i) {
+        total += i;
+        ++base;
+    }
+    return base;
+}
+
+
 vector<Point3f> split_triangle(vector<Point3f>* tri) {
   int i;
   int j;
 
   Point3f p;
   Point3f mp0, mp1, mp2;
-  vector<Point3f> newpoints;
-  for (i = 0; i < (*tri).size(); i += 3) {
-    mp0.x = ((*tri).at(i).x + (*tri).at(i + 1).x) / 2.0;
-    mp0.y = ((*tri).at(i).y + (*tri).at(i + 1).y) / 2.0;
-    mp0.z = ((*tri).at(i).z + (*tri).at(i + 1).z) / 2.0 + disp;
-    mp1.x = ((*tri).at(i + 1).x + (*tri).at(i + 2).x) / 2.0;
-    mp1.y = ((*tri).at(i + 1).y + (*tri).at(i + 2).y) / 2.0;
-    mp1.z = ((*tri).at(i + 1).z + (*tri).at(i + 2).z) / 2.0 + disp;
-    mp2.x = ((*tri).at(i + 2).x + (*tri).at(i).x) / 2.0;
-    mp2.y = ((*tri).at(i + 2).y + (*tri).at(i).y) / 2.0;
-    mp2.z = ((*tri).at(i + 2).z + (*tri).at(i).z) / 2.0 + disp;
 
-    newpoints.push_back(extract(&(*tri).at(i)));
-    newpoints.push_back(mp0);
-    newpoints.push_back(mp2);
-    newpoints.push_back(extract(&(*tri).at(i + 1)));
-    newpoints.push_back(mp0);
-    newpoints.push_back(mp1);
-    newpoints.push_back(extract(&(*tri).at(i + 2)));
-    newpoints.push_back(mp1);
-    newpoints.push_back(mp2);
-    newpoints.push_back(mp0);
-    newpoints.push_back(mp1);
-    newpoints.push_back(mp2);
+  int base = calc_base((*tri).size());
+  int nbase = 2*base - 1;
+
+  vector<Point3f> new_points;
+
+  int idx = 0;
+  for (i = base; i > 0; --i) {
+      // Insert new 'alternating' new-old row of points
+      for (j = 0; j < i; j++) {
+          // Take 'next' existing point not yet incorporated
+          // since the order will be the same in old & new arrays.
+          new_points.push_back((*tri).at(idx++));
+          if (j+1 != i) {
+              Point3f np;
+              np.is_new = true;
+              new_points.push_back(np);
+          }
+      }
+      // Is an all-new row required?
+      // Yes if i-1 != 1
+      // Need 2i-2
+      if (i > 1) {
+          for (j = 0; j < (2*i - 2); ++j) {
+              Point3f np;
+              np.is_new = true;
+              new_points.push_back(np);
+          }
+      }
+  }
+
+  cout << new_points.size() << " new points" << endl;
+
+
+  // New points inserted
+  // Now initialise them
+  int offset = 0;
+  for (i = nbase; i > 0; --i) {
+      for (j = 0; j < i; ++j) {
+          Point3f* p = &new_points.at(offset + j);
+          if (i % 2) {
+              // p might be new, and is in an
+              // alternating old-new point row
+              if (p->is_new) {
+                  Point3f pl = new_points.at(offset + j - 1);
+                  Point3f pr = new_points.at(offset + j + 1);
+                  p->x = (pl.x + pr.x) / 2.0;
+                  p->y = pl.y;
+                  p->z = (pl.z + pr.z) / 2.0;
+              }
+          } else {
+              // p is definitely new, as we're in
+              // a brand new point row
+              Point3f dl = new_points.at(offset + j - i - 1);
+              Point3f dr = new_points.at(offset + j - i    );
+              p->x = (dl.x + dr.x) / 2.0;
+              if (j % 2) {
+                  Point3f ul = new_points.at(offset + j + i - 1);
+                  p->y = (dr.y + ul.y) / 2.0;
+                  p->z = (dr.z + ul.z) / 2.0;
+              } else {
+                  Point3f ur = new_points.at(offset + j + i    );
+                  p->y = (dl.y + ur.y) / 2.0;;
+                  p->z = (dl.z + ur.z) / 2.0;;
+              }
+          }
+          p->is_new = false;
+      }
+      offset += i;
   }
 
   disp *= down;
-  return newpoints;
+  return new_points;
 }
 
 float* cross_product(Point3f a, Point3f b, Point3f c) {
@@ -166,7 +228,7 @@ void display() {
   glEnd();
   glFlush();
 
-  if /*(its < MAX_SUBDIVS)*/ (split) { ++its; split = false; tri = split_triangle(&tri); }
+  if /*(its < MAX_SUBDIVS)*/ (split) { ++its; split = false; tri = split_triangle(&tri); print_tri();}
 
   //SDL_Delay(1000);
 }
